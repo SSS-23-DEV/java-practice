cbdbad71047d0bfbf25cdd7e6bce475d567c553b
FredSteady23 <fred.steady@mail.ru>
2025-11-06 21:20:28 +0500

    решены задачи day3

--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -1,7 +1,46 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Day3 {
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//
+//        switch(input) {
+//            case "Москва", "Владивосток", "Ростов" -> System.out.println("Россия");
+//            case "Рим", "Милан", "Турин" -> System.out.println("Италия");
+//            case "Ливерпуль", "Манчестер", "Лондон" -> System.out.println("Англия");
+//            case "Берлин", "Мюнхен", "Кельн" -> System.out.println("Германия");
+//            case "Стоп" -> System.out.println(input);
+//            default -> System.out.println("Неизвестная страна");
+//        }
 
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        double a = scanner.nextDouble();
+//        double b = scanner.nextDouble();
+//
+//        if(a == 0 || b == 0){
+//            return;
+//        }
+//        double c = b / a;
+//
+//        System.out.println(c);
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//        for(int i=0; i<5; i++){
+//            double firstNumber = scanner.nextDouble();
+//            double secondNumber = scanner.nextDouble();
+//
+//            if(firstNumber == 0 || secondNumber == 0){
+//                System.out.println("Деление на ноль");
+//                continue;
+//            }
+//            double sum = firstNumber / secondNumber;
+//            System.out.println(sum);
+//        }
     }
 }
